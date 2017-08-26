608acdd0d84a7e6dd923c5b11961e6cd9a9fa465
Oliver Bell <freshollie@gmail.com>
2017-08-26 13:16:03 +0200

    Started adding source weblinks to build.py

--- build.py
+++ build.py
@@ -16,4 +16,11 @@ print("-"*20)
 os.system("cd panda3d")
 
 if not WINDOWS:
-    
+    pass
+
+# Ready to download these sources if they do not exist
+
+# https://github.com/freshollie/panda3d/archive/00150976f7b233b4be54e68a36785187a4bfa77b.zip
+# https://github.com/freshollie/Astron/archive/3a15606ab15b63b666fdff1e0145417470232dbc.zip
+# https://github.com/freshollie/toontown-infinite-resources/archive/f7cfe07d35893bf0c964e668bc46fb605e317d4d.zip
+#https://github.com/freshollie/toontown-infinite-source/archive/master.zip
